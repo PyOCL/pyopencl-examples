323a143fa5b9e77eac30c6624caf42072fecbdc7
John Hu <im@john.hu>
2018-02-07 10:18:31 +0800

    first version of 4-4 blockchain

---
+++ 4-4/blockchain.cl
@@ -0,0 +1,50 @@
+#include "sha256.cl"
+
+int calc_hash(__global unsigned char* data,
+              unsigned char* hash,
+              int data_length,
+              int difficulty,
+              long nonce) {
+    int i;
+    int matched;
+    SHA256_CTX ctx;
+    sha256_init(&ctx);
+    sha256_update(&ctx, nonce, data, data_length);
+	sha256_final(&ctx, hash);
+
+    matched = 1;
+    for (i = 0; i < difficulty && matched == 1; i++) {
+        if (hash[i] != 0) {
+            matched = 0;
+            break;
+        }
+    }
+    return matched;
+}
+
+__kernel void find_nonce(__global unsigned char* data,
+                         __global unsigned char* hash,
+                         volatile __global unsigned long* nonce,
+                         int data_length,
+                         int difficulty,
+                         int nonce_group_size) {
+
+    int global_id = get_global_id(0);
+    long nonce_start = nonce_group_size * global_id;
+    long nonce_end = nonce_start + nonce_group_size;
+    unsigned char local_hash[32];
+    int i, j;
+
+    for (i = nonce_start; i < nonce_end; i++) {
+        if (calc_hash(data, local_hash, data_length, difficulty, i) == 1) {
+            // TODO: need a atomic memory copy
+            printf("hash found: ");
+            for (j = 0; j < 32; j++) {
+                printf("%02x", local_hash[j]);
+            }
+            printf("\n");
+            atomic_xchg(nonce, i);
+            break;
+        }
+    }
+}
---
+++ 4-4/blockchain.py
@@ -0,0 +1,63 @@
+import os
+import math
+import numpy
+import pyopencl as cl
+import pyopencl.array
+import time
+
+if __name__ == '__main__':
+
+    print('load program from cl source file')
+    f = open('blockchain.cl', 'r', encoding='utf-8')
+    kernels = ''.join(f.readlines())
+    f.close()
+
+    print('prepare data ... ')
+    start_time = time.time()
+    # prepare host memory for OpenCL
+    data_str = 'abc'
+    input_data = numpy.fromstring(data_str, dtype=numpy.uint8)
+    output_data = numpy.zeros(32, dtype=numpy.uint8)
+    nonce = numpy.zeros(1, dtype=numpy.int64)
+    time_hostdata_loaded = time.time()
+
+    print('create context ...')
+    ctx = cl.create_some_context()
+    print('create command queue ...')
+    queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
+    time_ctx_queue_creation = time.time()
+
+    # prepare device memory for OpenCL
+    print('prepare device memory for input / output')
+    cl_input_data = cl.array.to_device(queue, input_data)
+    cl_output_data = cl.array.to_device(queue, output_data)
+    cl_nonce = cl.array.to_device(queue, nonce)
+    time_devicedata_loaded = time.time()
+
+    print('compile kernel code')
+    prg = cl.Program(ctx, kernels).build()
+    time_kernel_compilation = time.time()
+
+    print('execute kernel programs')
+    evt = prg.find_nonce(queue, (10240, ), (1, ), cl_input_data.data, cl_output_data.data, cl_nonce.data, numpy.int32(len(data_str)), numpy.int32(3), numpy.int32(1024))
+    print('wait for kernel executions')
+    evt.wait()
+    elapsed = 1e-9 * (evt.profile.end - evt.profile.start)
+
+    time_before_readback = time.time()
+    nonce = cl_nonce.get()
+    time_after_readback = time.time()
+
+    # hash_str = ''
+    # for i in range(32):
+    #     hash_str += '{:02x}'.format(output_hash[i])
+    print('=' * 20)
+    # print('hash: {}'.format(hash_str))
+    print('nonce: {}'.format(nonce))
+    print('=' * 20)
+
+    print('Prepare host data took       : {}'.format(time_hostdata_loaded - start_time))
+    print('Create CTX/QUEUE took        : {}'.format(time_ctx_queue_creation - time_hostdata_loaded))
+    print('Upload data to device took   : {}'.format(time_devicedata_loaded - time_ctx_queue_creation))
+    print('Compile kernel took          : {}'.format(time_kernel_compilation - time_devicedata_loaded))
+    print('OpenCL elapsed time          : {}'.format(elapsed))
